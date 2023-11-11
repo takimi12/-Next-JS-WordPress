92ebac1f2fd853511e1add8d1b437321118f28ba
takimi12 <tomek12olech@gmail.com>
2023-11-11 03:31:38 +0100

    Cover component, style with Tailwind CSS, & render images with Next JS Image

---
+++ components/BlockRenderer/BlockRenderer.js
@@ -0,0 +1,18 @@
+import { Cover } from "components/Cover/";
+
+export const BlockRenderer = ({blocks}) => {
+    return blocks.map((block) => {
+       switch(block.name) {
+        case "core/cover": {
+            return (
+            <Cover key={block.id} background={block.attributes.url}
+            >core coverdd</Cover>
+            )
+        }
+        default:
+            return null;
+    }
+});
+};
+ 
+export default BlockRenderer;
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,35 @@
-export default function Home() {
-  return <div>Next JS &amp; WordPress course.</div>;
+import { gql } from "@apollo/client";
+import client from "../client";
+import { cleanAndTransformBlocks } from "../utils/cleanAndTransformBlocks";
+import { BlockRenderer } from "components/BlockRenderer";
+
+export default function Home(props) {
+console.log(props);
+  return (
+    <div>
+<BlockRenderer blocks={props.blocks} />
+  </div>
+  )
+  ;
 }
+
+export const getStaticProps = async () => {
+  const {data} = await client.query({
+    query: gql`
+ query NewQuery {
+  nodeByUri(uri: "/") {
+    ... on Page {
+      id
+      blocks
+    }
+  }
+}
+    `,
+  });
+  return {
+    props: {
+      blocks: cleanAndTransformBlocks(data.nodeByUri.blocks),
+    
+    },
+  };
+};
