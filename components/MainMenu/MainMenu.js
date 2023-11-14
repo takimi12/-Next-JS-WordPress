38d4bad90a151766a114db2d51d2e7f9a55668c6
takimi12 <tomek12olech@gmail.com>
2023-11-14 22:39:06 +0100

    Create ButtonLink component & using the @apply Tailwind CSS directive

--- components/MainMenu/MainMenu.js
+++ components/MainMenu/MainMenu.js
@@ -1,3 +1,4 @@
+import { ButtonLink } from 'components/ButtonLink';
 import Link from 'next/link';
 import { FaHouseUser, FaHeart } from 'react-icons/fa';
 
@@ -41,11 +42,9 @@ export const MainMenu = ({
           </div>
         ))}
         <div className="ml-3 my-auto">
-          <Link href={callToActionDestination}>
-            <div className="bg-pink-500 hover:bg-pink-700 inline-block my-2 px-4 py-2 uppercase rounded-md cursor-pointer font-bold text-white">
-              {callToActionLabel}
-            </div>
-          </Link>
+        <ButtonLink 
+        destination={callToActionDestination}
+        label={callToActionLabel} />
         </div>
       </div>
     </div>
