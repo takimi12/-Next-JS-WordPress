3ad73fbaa7dff5080a3c96c2229bff290c91d215
takimi12 <tomek12olech@gmail.com>
2023-11-14 21:40:10 +0100

    Finish Menu

--- components/MainMenu/MainMenu.js
+++ components/MainMenu/MainMenu.js
@@ -1,7 +1,8 @@
+import Link from "next/link";
 import { FaHouseUser, FaHeart } from "react-icons/fa";
 
-export const MainMenu  = (props) => {
-    console.log("MainMenu props", props);
+export const MainMenu  = ({items}) => {
+    console.log("MainMenu props", items);
         return (  
         <div className="
         bg-slate-800
@@ -15,6 +16,33 @@ export const MainMenu  = (props) => {
         <div className="py-4 pl-5 flex text-pink-600">
             <FaHouseUser size={30} />
             <FaHeart size={30} />
+        </div>
+        <div className="flex flex-1 justify-end">
+            {(items || []).map(item => (
+                <div key={item.id} className="
+                hover:bg-slate-700 
+                cursor-pointer
+                relative
+                group">
+                    <div>
+                    <Link href={item.destination} className="p-5 block">
+                            {item.label}
+                        
+                    </Link>
+                    </div>
+                    {!!item.subMenuItems?.length &&(
+                     <div className=" group-hover:block hidden bg-slate-800 text-right absolute right-0 top-full -mt-3">
+                        {item.subMenuItems.map(subMenuItem =>(
+                        <Link className="block whitespace-nowrap p-5 hover:bg-slate-700"
+                         key={subMenuItem.id} href={subMenuItem.destination}>
+                                {subMenuItem.label}
+                           
+                        </Link>
+                       ))}
+                     </div>
+                     )}
+                     </div>
+            ))}
         </div>
             </div>
     );
