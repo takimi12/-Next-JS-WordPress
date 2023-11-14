d1ce0d902df85c627534602faf89887f7e09031d
takimi12 <tomek12olech@gmail.com>
2023-11-14 03:10:06 +0100

    Create additional pages & child pages in WordPress & dynamic routes in Next JS

---
+++ pages/[...slug].js
@@ -0,0 +1,35 @@
+import client from 'client';
+
+export default function Page(props) {
+    return <div>   Page</div>
+}
+
+export const getStaticProps = async () => {
+    return {
+        props: {},
+    }
+};
+
+
+export const getStaticPaths = async () => {
+const {data} = await client.query({
+    query: gql`
+    query AllPagesQuery {
+        pages {
+          nodes {
+            uri
+          }
+        }
+      }
+    `,
+});
+
+return {
+    paths: data.pages.nodes.filter(page => page.uri !== "/").map(page => ({
+        params:{
+            slug: page.uri.substring(1, page.uri.length -1).split('/'),
+        },
+    })),
+    fallback: "blocking",
+};
+};
