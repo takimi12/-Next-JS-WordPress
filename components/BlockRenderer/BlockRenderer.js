2931d600347d1f33be2fc170e3863ce40cc998d0
takimi12 <tomek12olech@gmail.com>
2023-11-14 16:38:48 +0100

    Create Paragraph component & further extend WordPress theme

--- components/BlockRenderer/BlockRenderer.js
+++ components/BlockRenderer/BlockRenderer.js
@@ -1,9 +1,20 @@
 import { Cover } from "components/Cover/";
 import {Heading} from "components/Heading/";
+import { Paragraph } from "components/Paragraph";
+import { theme } from "theme";
 
 export const BlockRenderer = ({blocks}) => {
     return blocks.map((block) => {
        switch(block.name) {
+        case 'core/paragraph': {
+            return <Paragraph 
+            key={block.id} 
+            textAlign={block.attributes.align}
+            content={block.attributes.content}
+            textColor={theme[block.attributes.textColor] ||
+            block.attributes.style?.color?.text}
+             />;
+        }
         case 'core/heading': {
             return <Heading 
             key={block.id} 
---
+++ components/Paragraph/Paragraph.js
@@ -0,0 +1,7 @@
+import { getTextAlign } from "utils/fonts";
+
+export const Paragraph = ({textAlign ="left",content, textColor}) => {
+return <p className={`max-w-5xl mx-auto ${getTextAlign(textAlign)}`}
+style={{textAlign, color: textColor}}
+ dangerouslySetInnerHTML={{__html:content}} />;
+};
