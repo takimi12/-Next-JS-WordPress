2d41c3cd5b9eee7ed206cd0273a29f6cb8848c8d
takimi12 <tomek12olech@gmail.com>
2023-11-21 23:30:37 +0100

    Create property features component in Next JS

--- components/BlockRenderer/BlockRenderer.js
+++ components/BlockRenderer/BlockRenderer.js
@@ -5,6 +5,7 @@ import { Cover } from "components/Cover/";
 import { FormspreeForm } from "components/FormspreeForm";
 import {Heading} from "components/Heading/";
 import { Paragraph } from "components/Paragraph";
+import { PropertyFeatures } from "components/PropertyFeatures";
 import { PropertySearch } from "components/PropertySearch";
 import Image from "next/image";
 import { theme } from "theme";
@@ -44,6 +45,18 @@ export const BlockRenderer = ({blocks}) => {
             level={block.attributes.level}
             content={block.attributes.content}
             textAlign={block.attributes} />;
+        }
+        case "acf/propertyfeatures": {
+            return <PropertyFeatures 
+            key={block.id} 
+            price={block.attributes.price}
+            bathrooms={block.attributes.bathrooms}
+            bedrooms={block.attributes.bedrooms}
+            hasParking={block.attributes.hasParking}
+            petFriendly={block.attributes.pet_friendly}
+            />;
+         
+            
         }
         case "acf/propertysearch": {
             return <PropertySearch key={block.id} />;
